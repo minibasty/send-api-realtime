1aecd7c759119ea7cd77436103d962daf5710ab6
bnt <fanta.orange_@hotmail.com>
2019-12-31 12:46:00 +0700

    add .gitignore

--- real_dlt.php
+++ real_dlt.php
@@ -24,7 +24,7 @@ function real_dlt($configKey, $configValue)
     `positions`.*
     FROM
     `devices`
-    INNER JOIN `positions` ON `positions`.`deviceid` = `devices`.`id` WHERE `devices`.`connect` = 1 AND `positions`.`id` > $configValue LIMIT 9000";
+    INNER JOIN `positions` ON `positions`.`deviceid` = `devices`.`id` WHERE `positions`.`id` > $configValue LIMIT 9000";
     $qr_positions = $conn->query($sql_position);
 
     $location_array = array();
@@ -32,74 +32,77 @@ function real_dlt($configKey, $configValue)
     $locations_countBackup = 0;
 
     while ($row_positions = $qr_positions->fetch_assoc()) {
-        // check protocol
-        $protocol_value = $row_positions['protocol'];
-        $ext_power_status = 0;
-        $position_id = $row_positions['id'];
-        $to_time = strtotime(date("Y-m-d H:i:s"));
-        $from_time = strtotime($row_positions['devicetime']);
+        if ($row_positions['connect'] == 1) {
 
-        // รหัส 4 ตัวท้ายเก็บจาก ฟิล type
-        $device_type = str_pad($row_positions['type'], 4, "0", STR_PAD_LEFT);
-        $vender_code = $vender_3bit . $device_type; //first unit_id
+            // check protocol
+            $protocol_value = $row_positions['protocol'];
+            $ext_power_status = 0;
+            $position_id = $row_positions['id'];
+            $to_time = strtotime(date("Y-m-d H:i:s"));
+            $from_time = strtotime($row_positions['devicetime']);
 
-        // คำนวณความต่างของเวลาเครื่องกับเวลาปุจจับัน
-        $time_interval = round(abs($to_time - $from_time) / 60, 2);
+            // รหัส 4 ตัวท้ายเก็บจาก ฟิล type
+            $device_type = str_pad($row_positions['type'], 4, "0", STR_PAD_LEFT);
+            $vender_code = $vender_3bit . $device_type; //first unit_id
 
-        if ($time_interval < 5) {
-            $locations_count++;
-            // ค่า attributes แปลง Json
-            $attributesJson = get_jsonattributes($row_positions['attributes']);
+            // คำนวณความต่างของเวลาเครื่องกับเวลาปุจจับัน
+            $time_interval = round(abs($to_time - $from_time) / 60, 2);
 
-            $driverLicense = get_driverId($row_positions['driverLicense']);  //driverLicense
+            if ($time_interval < 5) {
+                $locations_count++;
+                // ค่า attributes แปลง Json
+                $attributesJson = get_jsonattributes($row_positions['attributes']);
 
-            $date_utc_ts = dateTimeUTC($row_positions['devicetime']); //date_utc_ts
-            $date_recv_utc_ts = dateTimeUTC($row_positions['servertime']);  //date_recv_utc_ts
+                $driverLicense = get_driverId($row_positions['driverLicense']);  //driverLicense
 
-            if ($row_positions['dlt']) {
-                $imei = $row_positions['dlt'];
-            } else {
-                $imei = $row_positions['uniqueid'];
-            }
+                $date_utc_ts = dateTimeUTC($row_positions['devicetime']); //date_utc_ts
+                $date_recv_utc_ts = dateTimeUTC($row_positions['servertime']);  //date_recv_utc_ts
 
-            $unit_id = get_unitId($vender_code, $imei); //unit_id
-            $hdop = property_exists($attributesJson, 'hdop') ? round($attributesJson->hdop) : 0; //hdop
-
-
-            if (property_exists($attributesJson, 'status')) {
-                if ($attributesJson->status == 2400 || $attributesJson->status == 6400 || $attributesJson->status == 2000) {
-                    $ext_power_status = 1;
+                if ($row_positions['dlt']) {
+                    $imei = $row_positions['dlt'];
+                } else {
+                    $imei = $row_positions['uniqueid'];
                 }
-            }
 
-            $engine_status = getEngineStatus($protocol_value, $attributesJson);
+                $unit_id = get_unitId($vender_code, $imei); //unit_id
+                $hdop = property_exists($attributesJson, 'hdop') ? round($attributesJson->hdop) : 0; //hdop
 
-            array_push($location_array, array(
-                'driver_id' => $driverLicense,
-                'unit_id' => $unit_id,
-                'seq' => '0',
-                'utc_ts' => $date_utc_ts,
-                'recv_utc_ts' => $date_recv_utc_ts,
-                'lat' => $row_positions['latitude'],
-                'lon' => $row_positions['longitude'],
-                'alt' => $row_positions['altitude'],
-                "speed" => intval($row_positions['speed'] * 1.852),
-                "engine_status" => $engine_status,
-                "fix" => 0,
-                "license" => $imei,
-                "course" => $row_positions['course'],
-                "hdop" => $hdop,
-                "num_sats" => 0,
-                "gsm_cell" => 0,
-                "gsm_loc" => 0,
-                "gsm_rssi" => 0,
-                "mileage" => 0,
-                "ext_power_status" => $ext_power_status,
-                "ext_power" => 0,
-                "high_acc_count" => "",
-                "high_de_acc_count" => "",
-            ));
-        } // end if
+
+                if (property_exists($attributesJson, 'status')) {
+                    if ($attributesJson->status == 2400 || $attributesJson->status == 6400 || $attributesJson->status == 2000) {
+                        $ext_power_status = 1;
+                    }
+                }
+
+                $engine_status = getEngineStatus($protocol_value, $attributesJson);
+
+                array_push($location_array, array(
+                    'driver_id' => $driverLicense,
+                    'unit_id' => $unit_id,
+                    'seq' => '0',
+                    'utc_ts' => $date_utc_ts,
+                    'recv_utc_ts' => $date_recv_utc_ts,
+                    'lat' => $row_positions['latitude'],
+                    'lon' => $row_positions['longitude'],
+                    'alt' => $row_positions['altitude'],
+                    "speed" => intval($row_positions['speed'] * 1.852),
+                    "engine_status" => $engine_status,
+                    "fix" => 0,
+                    "license" => $imei,
+                    "course" => $row_positions['course'],
+                    "hdop" => $hdop,
+                    "num_sats" => 0,
+                    "gsm_cell" => 0,
+                    "gsm_loc" => 0,
+                    "gsm_rssi" => 0,
+                    "mileage" => 0,
+                    "ext_power_status" => $ext_power_status,
+                    "ext_power" => 0,
+                    "high_acc_count" => "",
+                    "high_de_acc_count" => "",
+                ));
+            } // end if
+        }
     } //end while
     if ($locations_count) {
         $data_array = array('vender_id' => $vender_id, 'locations_count' => $locations_count, 'locations' => $location_array);
@@ -112,6 +115,6 @@ function real_dlt($configKey, $configValue)
     $sql_updateConfig = "UPDATE config SET value = $position_id WHERE `key` = '$configKey'";
     $qr_updateConfig = $conn->query($sql_updateConfig);
     // print_r($send_res);
-    
+
     // $res_Data = $send_res . $data_arrayJson;
 } //end function
--- real_dlt2.php
+++ real_dlt2.php
@@ -24,7 +24,7 @@ function real_dlt2($configKey, $configValue)
     `positions`.*
     FROM
     `devices`
-    INNER JOIN `positions` ON `positions`.`deviceid` = `devices`.`id` WHERE `devices`.`connect_dlt2` = 1 AND `positions`.`id` > $configValue LIMIT 9000";
+    INNER JOIN `positions` ON `positions`.`deviceid` = `devices`.`id` WHERE `positions`.`id` > $configValue LIMIT 9000";
     $qr_positions = $conn->query($sql_position);
 
     $location_array = array();
@@ -32,77 +32,80 @@ function real_dlt2($configKey, $configValue)
     $locations_countBackup = 0;
 
     while ($row_positions = $qr_positions->fetch_assoc()) {
-        // check protocol
-        $protocol_value = $row_positions['protocol'];
-        $ext_power_status = 0;
-        $position_id = $row_positions['id'];
-        $to_time = strtotime(date("Y-m-d H:i:s"));
-        $from_time = strtotime($row_positions['devicetime']);
+        if ($row_positions['connect_dlt2'] == 1) {
 
-        // รหัส 4 ตัวท้ายเก็บจาก ฟิล type
-        $device_type = str_pad($row_positions['type'], 4, "0", STR_PAD_LEFT);
-        $vender_code = $vender_3bit . $device_type; //first unit_id
+            // check protocol
+            $protocol_value = $row_positions['protocol'];
+            $ext_power_status = 0;
+            $position_id = $row_positions['id'];
+            $to_time = strtotime(date("Y-m-d H:i:s"));
+            $from_time = strtotime($row_positions['devicetime']);
 
-        // คำนวณความต่างของเวลาเครื่องกับเวลาปุจจับัน
-        $time_interval = round(abs($to_time - $from_time) / 60, 2);
+            // รหัส 4 ตัวท้ายเก็บจาก ฟิล type
+            $device_type = str_pad($row_positions['type'], 4, "0", STR_PAD_LEFT);
+            $vender_code = $vender_3bit . $device_type; //first unit_id
 
-        if ($time_interval < 5) {
-            $locations_count++;
-            // ค่า attributes แปลง Json
-            $attributesJson = get_jsonattributes($row_positions['attributes']);
+            // คำนวณความต่างของเวลาเครื่องกับเวลาปุจจับัน
+            $time_interval = round(abs($to_time - $from_time) / 60, 2);
 
-            
-            if (property_exists($attributesJson, 'driverUniqueId')) {
-                $driverLicense = get_driverId($attributesJson->driverUniqueId);  //driverLicense
-            }
-
-            $date_utc_ts = dateTimeUTC($row_positions['devicetime']); //date_utc_ts
-            $date_recv_utc_ts = dateTimeUTC($row_positions['servertime']);  //date_recv_utc_ts
-
-            if ($row_positions['dlt']) {
-                $imei = $row_positions['dlt'];
-            } else {
-                $imei = $row_positions['uniqueid'];
-            }
-
-            $unit_id = get_unitId($vender_code, $imei); //unit_id
-            $hdop = property_exists($attributesJson, 'hdop') ? round($attributesJson->hdop) : 0; //hdop
+            if ($time_interval < 5) {
+                $locations_count++;
+                // ค่า attributes แปลง Json
+                $attributesJson = get_jsonattributes($row_positions['attributes']);
 
 
-            if (property_exists($attributesJson, 'status')) {
-                if ($attributesJson->status == 2400 || $attributesJson->status == 6400 || $attributesJson->status == 2000) {
-                    $ext_power_status = 1;
+                if (property_exists($attributesJson, 'driverUniqueId')) {
+                    $driverLicense = get_driverId($attributesJson->driverUniqueId);  //driverLicense
                 }
-            }
 
-            $engine_status = getEngineStatus($protocol_value, $attributesJson);
+                $date_utc_ts = dateTimeUTC($row_positions['devicetime']); //date_utc_ts
+                $date_recv_utc_ts = dateTimeUTC($row_positions['servertime']);  //date_recv_utc_ts
 
-            array_push($location_array, array(
-                'driver_id' => $driverLicense,
-                'unit_id' => $unit_id,
-                'seq' => '0',
-                'utc_ts' => $date_utc_ts,
-                'recv_utc_ts' => $date_recv_utc_ts,
-                'lat' => $row_positions['latitude'],
-                'lon' => $row_positions['longitude'],
-                'alt' => $row_positions['altitude'],
-                "speed" => intval($row_positions['speed'] * 1.852),
-                "engine_status" => $engine_status,
-                "fix" => 0,
-                "license" => $imei,
-                "course" => $row_positions['course'],
-                "hdop" => $hdop,
-                "num_sats" => 0,
-                "gsm_cell" => 0,
-                "gsm_loc" => 0,
-                "gsm_rssi" => 0,
-                "mileage" => 0,
-                "ext_power_status" => $ext_power_status,
-                "ext_power" => 0,
-                "high_acc_count" => "",
-                "high_de_acc_count" => "",
-            ));
-        } // end if
+                if ($row_positions['dlt']) {
+                    $imei = $row_positions['dlt'];
+                } else {
+                    $imei = $row_positions['uniqueid'];
+                }
+
+                $unit_id = get_unitId($vender_code, $imei); //unit_id
+                $hdop = property_exists($attributesJson, 'hdop') ? round($attributesJson->hdop) : 0; //hdop
+
+
+                if (property_exists($attributesJson, 'status')) {
+                    if ($attributesJson->status == 2400 || $attributesJson->status == 6400 || $attributesJson->status == 2000) {
+                        $ext_power_status = 1;
+                    }
+                }
+
+                $engine_status = getEngineStatus($protocol_value, $attributesJson);
+
+                array_push($location_array, array(
+                    'driver_id' => $driverLicense,
+                    'unit_id' => $unit_id,
+                    'seq' => '0',
+                    'utc_ts' => $date_utc_ts,
+                    'recv_utc_ts' => $date_recv_utc_ts,
+                    'lat' => $row_positions['latitude'],
+                    'lon' => $row_positions['longitude'],
+                    'alt' => $row_positions['altitude'],
+                    "speed" => intval($row_positions['speed'] * 1.852),
+                    "engine_status" => $engine_status,
+                    "fix" => 0,
+                    "license" => $imei,
+                    "course" => $row_positions['course'],
+                    "hdop" => $hdop,
+                    "num_sats" => 0,
+                    "gsm_cell" => 0,
+                    "gsm_loc" => 0,
+                    "gsm_rssi" => 0,
+                    "mileage" => 0,
+                    "ext_power_status" => $ext_power_status,
+                    "ext_power" => 0,
+                    "high_acc_count" => "",
+                    "high_de_acc_count" => "",
+                ));
+            } // end if
+        }
     } //end while
     if ($locations_count) {
         $data_array = array('vender_id' => $vender_id, 'locations_count' => $locations_count, 'locations' => $location_array);
